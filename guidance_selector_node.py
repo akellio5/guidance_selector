250e9f50a92dcabb0ed2035c01378d5ddd72835d
akellio5 <kirick1120044@gmail.com>
2025-02-23 01:04:13 +0200

    New value/change old value

--- guidance_selector_node.py
+++ guidance_selector_node.py
@@ -14,22 +14,32 @@ class GuidanceSelectorNode:
             }
         }
 
-    RETURN_TYPES = ("FLOAT",)
+    RETURN_TYPES = ("FLOAT", "BOOLEAN",) 
     FUNCTION = "calculate_guidance"
     CATEGORY = "Custom Nodes/Guidance"
 
     def calculate_guidance(self, guidance_value):
         value = max(0.0, min(100.0, guidance_value))
+        
+
         if value >= 50:
-            output = 1.5
+            output_float = 1.5
         elif value >= 40:
-            output = 1.8
+            output_float = 1.7
         elif value >= 10:
-            output = 2.0
+            output_float = 1.9
         elif value >= 5.5:
-            output = 2.3
+            output_float = 2.2
         elif value >= 4:
-            output = 2.8
+            output_float = 3.2
+        elif value >= 2:
+            output_float = 3.7
         else:
-            output = 2.9
-        return (output,)
+            output_float = 5
+
+        output_bool = value < 4.0
+
+        return (output_float, output_bool,)
+
+NODE_CLASS_MAPPINGS = {"GuidanceSelector": GuidanceSelectorNode}
+NODE_DISPLAY_NAME_MAPPINGS = {"GuidanceSelector": "Guidance Selector"}
