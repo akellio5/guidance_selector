3adc206b9b13b305b08b73891d03ab6dfa11ca7e
akellio5 <kirick1120044@gmail.com>
2025-02-24 00:32:38 +0200

    guidance2

--- guidance_selector_node.py
+++ guidance_selector_node.py
@@ -12,37 +12,55 @@ class GuidanceSelectorNode:
             }
         }
 
-    RETURN_TYPES = ("FLOAT", "BOOLEAN",) 
+    RETURN_TYPES = ("FLOAT", "BOOLEAN", "FLOAT",)
     FUNCTION = "calculate_guidance"
     CATEGORY = "Custom Nodes/Guidance"
+
+    def calculate_output(self, input_value):
+        if input_value < 0.5:
+            return 5.0
+        elif 0.5 <= input_value < 5:
+            upper, lower = 5, 0.5
+            out_start, out_end = 3.2, 5.0
+        elif 5 <= input_value < 10:
+            upper, lower = 10, 5
+            out_start, out_end = 2.2, 3.2
+        elif 10 <= input_value < 40:
+            upper, lower = 40, 10
+            out_start, out_end = 1.9, 2.2
+        elif 40 <= input_value < 50:
+            upper, lower = 50, 40
+            out_start, out_end = 1.7, 1.9
+        elif 50 <= input_value <= 100:
+            upper, lower = 100, 50
+            out_start, out_end = 1.5, 1.7
+        else:
+            return None
+        
+        output = out_start + (upper - input_value) * (out_end - out_start) / (upper - lower)
+        return round(output, 1)
+
+    def calculate_denoise(self, input_value):
+        input_value = max(0.0, min(100.0, input_value))
+        if 40 <= input_value <= 100:
+            return 0.0
+        elif 10 <= input_value < 40:
+            denoise = (40 - input_value) * 0.1 / 30
+            return round(denoise, 2)
+        elif 0 <= input_value < 10:
+            denoise = 0.1 + (10 - input_value) * 0.5 / 10
+            return round(denoise, 2)
+        else:
+            return 0.0
 
     def calculate_guidance(self, guidance_value):
         value = max(0.0, min(100.0, guidance_value))
         
-        # Сегменты для интерполяции (x_high, y_high, x_low, y_low)
-        segments = [
-            (50.0, 1.5, 40.0, 1.7),
-            (40.0, 1.7, 10.0, 1.9),
-            (10.0, 1.9, 5.5, 2.2),
-            (5.5, 2.2, 4.0, 3.2),
-            (4.0, 3.2, 2.0, 3.7),
-        ]
-
-        if value >= 50.0:
-            output_float = 1.5
-        elif value < 2.0:
-            output_float = 5.0
-        else:
-            for x_high, y_high, x_low, y_low in segments:
-                if x_low <= value < x_high:
-                    slope = (y_low - y_high) / (x_low - x_high)
-                    output_float = y_high + slope * (value - x_high)
-                    break
-            output_float = round(output_float, 1)
-
+        output_float = self.calculate_output(value)
         output_bool = value < 4.0
+        denoise_value = self.calculate_denoise(value)
 
-        return (output_float, output_bool,)
+        return (output_float, output_bool, denoise_value,)
 
 NODE_CLASS_MAPPINGS = {"GuidanceSelector": GuidanceSelectorNode}
 NODE_DISPLAY_NAME_MAPPINGS = {"GuidanceSelector": "Guidance Selector"}
